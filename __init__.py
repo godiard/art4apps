1db16d0c208dd2528200977b94b2f9cb2b2132c3
Gonzalo Odiard <godiard@gmail.com>
2013-12-13 17:55:59 -0300

    Improve api, add support to audio files
    Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ import os
 
 DATA_PATH = '/usr/share/art4apps/data/'
 IMAGES_PATH = '/usr/share/art4apps/images/'
+AUDIO_PATH = '/usr/share/art4apps/audio/'
 
 
 class Art4Apps:
@@ -17,46 +18,74 @@ class Art4Apps:
 
     def _init_words(self):
         if self._words is None:
-            with open(os.path.join(DATA_PATH, 'words.json')) as json_file:
+            if os.path.exists('./data/words.json'):
+                json_path = './data/words.json'
+            else:
+                json_path = os.path.join(DATA_PATH, 'words.json')
+
+            with open(json_path) as json_file:
                 self._words = json.load(json_file)
 
     def _init_categories_words(self):
         if self._categories_words is None:
-            with open(os.path.join(
-                    DATA_PATH, 'categories_words.json')) as json_file:
+            if os.path.exists('./data/categories_words.json'):
+                json_path = './data/categories_words.json'
+            else:
+                json_path = os.path.join(DATA_PATH, 'categories_words.json')
+
+            with open(json_path) as json_file:
                 self._categories_words = json.load(json_file)
 
     def _init_languages(self):
         if self._languages is None:
             self._languages = []
-            for file_name in os.listdir(DATA_PATH):
-                if file_name.startswith('words_'):
-                    language = file_name[6:8]
-                    self._languages.append(language)
+            data_path_list = ['./data/', DATA_PATH]
+            for data_path in data_path_list:
+                for file_name in os.listdir(DATA_PATH):
+                    if file_name.startswith('words_'):
+                        language = file_name[6:8]
+                        if not language in self._languages:
+                            self._languages.append(language)
 
     def _init_translation_language(self, language):
         if language not in self.get_languages():
             raise Exception('language no available')
         if language not in self._translations:
-            translation_json_path = os.path.join(DATA_PATH,
-                                                 'words_%s.json' % language)
-            with open(translation_json_path) as json_file:
+            if os.path.exists('./data/words_%s.json' % language):
+                json_path = './data/words_%s.json' % language
+            else:
+                json_path = os.path.join(DATA_PATH,
+                                         'words_%s.json' % language)
+
+            with open(json_path) as json_file:
                 self._translations[language] = json.load(json_file)
 
-    def get_image_filename_by_word(self, word):
+    def get_image_filename(self, word):
         self._init_words()
         try:
-            return IMAGES_PATH + self._words[word]
+            return os.path.join(IMAGES_PATH, self._words[word])
         except:
+            return None
+
+    def get_audio_filename(self, word, language='en'):
+        self._init_words()
+        audio_file_path = os.path.join(AUDIO_PATH, language, "%s.ogg" % word)
+        if os.path.exists(audio_file_path):
+            return audio_file_path
+        else:
             return None
 
     def get_categories(self):
         self._init_categories_words()
         return self._categories_words.keys()
 
-    def get_words(self):
+    def get_words(self, language='en'):
         self._init_words()
-        return self._words.keys()
+        self._init_languages()
+        if language == 'en':
+            return self._words.keys()
+        else:
+            return self._translations[language].values()
 
     def get_words_by_category(self, category):
         self._init_categories_words()
@@ -66,14 +95,14 @@ class Art4Apps:
         self._init_languages()
         return self._languages
 
-    def get_translation(self, language, word):
+    def get_translation(self, word, language):
         self._init_translation_language(language)
         try:
             return self._translations[language][word]
         except:
             return None
 
-    def get_english_for(self, language, word):
+    def get_english_for(self, word, language):
         self._init_translation_language(language)
         translations = self._translations[language]
         for key in translations.keys():
